dced69000f5581eb231f806131ec5957ddde4513
Sam <samzaldivar98@gmail.com>
2018-09-19 22:45:59 -0400

    Added lesson 2: variables

---
+++ C++ Tutorial Dev/C++TutorialDev/C++TutorialDev/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 10;
+    cout << a << endl;
+    
+    int b = 5;
+    cout << b << endl;
+    
+    cout << a + b << endl;
+    
+    int c = a + b;
+    cout << c << endl;œ
+    
+    double pi = 3.14;
+    cout << pi << endl;
+    
+    pi = pi + c;
+    
+    cout << pi << endl;
+}
---
+++ C++ Tutorial/Lesson 2: Variables/variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    cout << a << endl;
+    
+    int b = 5;
+    cout << b << endl;
+    
+    cout << a + b << endl;
+    
+    int c = a + b;
+    cout << c << endl;
+    
+    double pi = 3.14;
+    cout << pi << endl;
+    
+    pi = pi + b;
+    cout << pi << endl;
+    
+    a = a + 2;
+    cout << a << endl;
+}
